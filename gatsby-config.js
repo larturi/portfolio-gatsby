b617aa7da4f357de119c7102d4f94f2ea72ef4af
lea.arturi <lea.arturi@gmail.com>
2021-07-25 15:40:38 -0300

    Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './index.scss';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <p>Detalle</p>
     </div>
   );
 };
