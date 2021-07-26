7118d2196b019fcd863b4dd96a4f5f1f318eca5d
lea.arturi <lea.arturi@gmail.com>
2021-07-26 13:19:08 -0300

    Services (WIP)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { graphql } from 'gatsby';
 import Layout from '../layouts/Layout/Layout';
 import Hero from '../components/Hero';
-// import Services from "../components/Services"
+import Services from '../components/Services';
 // import Jobs from "../components/Jobs"
 // import Projects from "../components/Projects"
 // import Blogs from "../components/Blogs"
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <Layout>
       <Hero />
+      <Services />
     </Layout>
   );
 };
