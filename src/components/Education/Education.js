ff305bfcc9aeda483fdc3cf51b77ff883b4d3afa
lea.arturi <lea.arturi@gmail.com>
2021-07-27 18:25:56 -0300

    Traducciones

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -58,20 +58,58 @@ const query = graphql`
         id
       }
     }
+    titleEducationEN: strapi {
+      navs(locale: "en", where: { url: "/education" }) {
+        text
+      }
+    }
+    titleEducationES: strapi {
+      navs(locale: "es-AR", where: { url: "/education" }) {
+        text
+      }
+    }
+    titleCoursesEN: strapi {
+      translates(locale: "en", where: { slug: "cursos" }) {
+        text
+      }
+    }
+    titleCoursesES: strapi {
+      translates(locale: "es-AR", where: { slug: "cursos" }) {
+        text
+      }
+    }
   }
 `;
 
 const Education = () => {
   const state = useContext(GlobalStateContext);
-  const { coursesEN, coursesES, degreeEN, degreeES } = useStaticQuery(query);
+  const {
+    coursesEN,
+    coursesES,
+    degreeEN,
+    degreeES,
+    titleEducationEN,
+    titleEducationES,
+    titleCoursesEN,
+    titleCoursesES,
+  } = useStaticQuery(query);
+
   const degrees =
     state.selectedLang === 'es-AR' ? degreeES.educations : degreeEN.educations;
   const courses =
     state.selectedLang === 'es-AR' ? coursesES.cursos : coursesEN.cursos;
+  const titleEducation =
+    state.selectedLang === 'es-AR'
+      ? titleEducationES.navs[0].text
+      : titleEducationEN.navs[0].text;
+  const titleCourses =
+    state.selectedLang === 'es-AR'
+      ? titleCoursesES.translates[0].text
+      : titleCoursesEN.translates[0].text;
 
   return (
     <section className="section educations">
-      <Title title="Education" />
+      <Title title={titleEducation} />
       <div className="section-center-50">
         {degrees.map((degree, index) => {
           return <Degree key={degree.order} index={index} {...degree} />;
@@ -80,7 +118,7 @@ const Education = () => {
 
       <div className="education-separador" />
 
-      <Title title="Cursos" />
+      <Title title={titleCourses} />
       <div className="section-center-50">
         {courses.map((course, index) => {
           return <Course key={index} index={index} {...course} />;
