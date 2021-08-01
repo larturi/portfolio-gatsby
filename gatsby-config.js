f4496865b73bd3b2e4c51b61808fad4cf57fda76
lea.arturi <lea.arturi@gmail.com>
2021-08-01 12:29:20 -0300

    Fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Leandro Arturi`,
+        short_name: `Home`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -42,43 +42,100 @@ module.exports = {
         icon: `src/images/favicon.png`,
         icons: [
           {
-            src: `/favicons/icon-48x48.png`,
+            src: `/favicons/android-icon-36x36.png`,
+            sizes: `36x36`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-icon-48x48.png`,
             sizes: `48x48`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-72x72.png`,
+            src: `/favicons/android-icon-72x72.png`,
             sizes: `72x72`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-96x96.png`,
+            src: `/favicons/android-icon-96x96.png`,
             sizes: `96x96`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-144x144.png`,
+            src: `/favicons/android-icon-144x144.png`,
             sizes: `144x144`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-192x192.png`,
+            src: `/favicons/android-icon-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
+
           {
-            src: `/favicons/icon-256x256.png`,
-            sizes: `256x256`,
+            src: `/favicons/apple-icon-57x57.png`,
+            sizes: `57x57`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-384x384.png`,
-            sizes: `384x384`,
+            src: `/favicons/apple-icon-60x60.png`,
+            sizes: `60x60`,
             type: `image/png`,
           },
           {
-            src: `/favicons/icon-512x512.png`,
-            sizes: `512x512`,
+            src: `/favicons/apple-icon-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-76x76.png`,
+            sizes: `76x76`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-114x114.png`,
+            sizes: `114x114`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-120x120.png`,
+            sizes: `120x120`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-152x152.png`,
+            sizes: `152x152`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon-180x180.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/apple-icon.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+
+          {
+            src: `/favicons/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/favicon-96x96.png`,
+            sizes: `96x96`,
             type: `image/png`,
           },
         ],
