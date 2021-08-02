305f52969b6f46d292024aa9d7046dca0c35bfbc
lea.arturi <lea.arturi@gmail.com>
2021-08-02 14:30:31 -0300

    [DarkMode] Github Cal

--- src/components/GithubCal/GithubCal.js
+++ src/components/GithubCal/GithubCal.js
@@ -1,14 +1,26 @@
 import './GithubCal.scss';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
 
+import { GlobalStateContext } from '../../context/GlobalContextProvider';
+
 const GithubCal = () => {
+  const state = useContext(GlobalStateContext);
+
+  let currentLanguaje = state.language;
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('locale'))
+      currentLanguaje = localStorage.getItem('locale');
+  }
+
+  const theme = currentLanguaje === 'es-AR' ? 'dark' : 'light';
+
   return (
     <GitHubCalendar
       username="larturi"
-      color="hsl(185, 81%, 29%)"
+      color={theme === 'dark' ? '#4f348d' : 'hsl(185, 81%, 29%)'}
       blockSize={12}
       blockMargin={4}
     >
