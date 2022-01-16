fc3462886de105d0068f6fb00aea91aa4ae84a0e
lea.arturi <lea.arturi@gmail.com>
2022-01-16 16:28:11 -0300

    Update Principal Technologies

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -90,16 +90,19 @@ const About = () => {
             theme={currentTheme === 'dark' ? 'dark' : 'light'}
           />
           <div className={`about-stack ${currentTheme}`}>
-            <span><IconoFa name='FaReact'/>React</span>
             <span><IconoFa name='SiDjango'/>Django</span>
             <span><IconoFa name='FaPython'/>Python</span>
             <span><IconoFa name='FaLaravel'/>Laravel</span>
             <span><IconoFa name='SiPhp'/>Php</span>
             <span><IconoFa name='FaNodeJs'/>Node</span>
+            <span><IconoFa name='SiJavascript'/>JavaScript</span>
             <span><IconoFa name='SiGraphql'/>GraphQL</span>
+            <span><IconoFa name='FaReact'/>React</span>
             <span><IconoFa name='SiSass'/>Sass</span>
             <span><IconoFa name='SiBootstrap'/>Bootstrap</span>
             <span><IconoFa name='SiPostgresql'/>PostgreSQL</span>
+            <span><IconoFa name='SiMysql'/>MySQL</span>
+            <span><IconoFa name='SiMongodb'/>MongoDB</span>
             <span><IconoFa name='SiDocker'/>Docker</span>
             <span><IconoFa name='FaGithub'/>GitHub</span>
           </div>
