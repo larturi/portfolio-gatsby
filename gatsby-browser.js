2f62013c0a5faff1bda448c65c2cf1474a38fdb8
Leandro Arturi <lea.arturi@gmail.com>
2022-02-05 19:37:02 -0300

    Optimizaciones

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,15 +6,15 @@
 
 // You can delete this file if you're not using it
 
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 import './src/sass/global.scss';
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
+    const answer = window.confirm(
+        `This application has been updated. ` +
+        `Reload to display the latest version?`
+    );
+    if (answer === true) {
+        window.location.reload();
+    }
 };
